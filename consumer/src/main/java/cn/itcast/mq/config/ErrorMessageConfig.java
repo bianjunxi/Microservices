95d260d53560b13430278d00f9ff4d662dfb65b4
Jay <bjxstart@gmail.com>
2024-05-16 22:58:40 +0800

    消费者失败消息处理策略

---
+++ consumer/src/main/java/cn/itcast/mq/config/ErrorMessageConfig.java
@@ -0,0 +1,45 @@
+package cn.itcast.mq.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.retry.MessageRecoverer;
+import org.springframework.amqp.rabbit.retry.RepublishMessageRecoverer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ClassName:  ErrorMessageConfig
+ * Description:
+ *
+ * @author Jay
+ * @version v1.0
+ */
+@Configuration
+public class ErrorMessageConfig {
+
+    @Bean
+    public DirectExchange errorMessageExchange(){
+        return new DirectExchange("error.direct");
+    }
+    @Bean
+    public Queue errorQueue(){
+        return new Queue("error.queue");
+    }
+
+    @Bean
+    public Binding errorMessageBinding(){
+        return BindingBuilder.bind(errorQueue()).to(errorMessageExchange()).with("error");
+    }
+
+    @Bean
+    public MessageRecoverer republishMessageRecoverer(RabbitTemplate rabbitTemplate){
+        return new RepublishMessageRecoverer(rabbitTemplate,"error.direct","error");
+    }
+
+    //在界面管理端直接发送消息,消费者监听并且重试后,会将异常的消息发送到异常交换机和队列中
+
+}
