ba488dfe1ad1b557d3f65bc1e92f3b75c03c88e2
Jay <bjxstart@gmail.com>
2024-05-16 22:37:38 +0800

    消费者消息确认

--- consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
+++ consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
@@ -1,13 +1,19 @@
 package cn.itcast.mq.listener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringRabbitListener {
 
+    private static final Logger log = LoggerFactory.getLogger(SpringRabbitListener.class);
+
     @RabbitListener(queues = "simple.queue")
     public void listenSimpleQueue(String msg) {
         System.out.println("消费者接收到simple.queue的消息：【" + msg + "】");
+        System.out.println(1/0);
+        log.info("消费者消息处理成功！");
     }
 }
