572cba146006a8fd478c7495163a423f1eb1554a
Jay <bjxstart@gmail.com>
2024-05-16 22:41:22 +0800

    消费者失败重试retry机制

--- consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
+++ consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitListener.java
@@ -12,7 +12,10 @@ public class SpringRabbitListener {
 
     @RabbitListener(queues = "simple.queue")
     public void listenSimpleQueue(String msg) {
-        System.out.println("消费者接收到simple.queue的消息：【" + msg + "】");
+        //debug：方便查看消息重试对应的时间，
+        //重试次数超过4次，会Spring会返回reject拒接，rabbitmq丢弃消息
+        //丢弃消息可以接受，因为本次重试多次依然不能成功，即便返回nack，重新投递本地未必可以成功
+        log.debug("消费者接收到simple.queue的消息：【" + msg + "】");
         System.out.println(1/0);
         log.info("消费者消息处理成功！");
     }
